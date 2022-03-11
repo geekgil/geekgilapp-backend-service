2f15e320c2d74fe5bae6f160f2fb2296ba35cdad
Gil Vale <geekgil2015@gmail.com>
2022-03-11 03:30:02 -0300

    deus me ajude a fazer o front end pegar :,(

--- src/main/java/com/example/demo/EmployeeController.java
+++ src/main/java/com/example/demo/EmployeeController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://edumobility.herokuapp.com")
+@CrossOrigin(origins = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
